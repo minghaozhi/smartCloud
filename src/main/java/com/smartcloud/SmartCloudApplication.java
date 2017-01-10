2aada6d2f0be596d961f6e2b8ff84c19a13b5b81
minghaozhi <597575122@qq.com>
2017-01-10 16:29:02 +0800

    springboot 通用Mapper

---
+++ src/main/java/com/smartcloud/SmartCloudApplication.java
@@ -0,0 +1,12 @@
+package com.smartcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCloudApplication.class, args);
+	}
+}
