3edec9a7fa89f0674486af9f6d44cc637b300027
minghaozhi <597575122@qq.com>
2017-01-10 17:37:12 +0800

    配置文件

--- src/main/java/com/smartcloud/SmartCloudApplication.java
+++ src/main/java/com/smartcloud/SmartCloudApplication.java
@@ -1,6 +1,6 @@
 package com.smartcloud;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
