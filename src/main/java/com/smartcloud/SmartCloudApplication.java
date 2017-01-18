a82161ac7653961ed01e9ae0d31d2bcde8c7a1cc
minghaozhi <597575122@qq.com>
2017-01-18 18:19:53 +0800

    2017.01.17

---
+++ src/main/java/com/smartcloud/SmartCloudApplication.java
@@ -0,0 +1,14 @@
+package com.smartcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SmartCloudApplication  extends SpringBootServletInitializer {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCloudApplication.class, args);
+	}
+}
