4fb18e751ad73b7ba73b2230b8a31cd977c89bca
minghaozhi <597575122@qq.com>
2017-01-19 09:24:46 +0800

    登录页面

--- src/main/java/com/smartcloud/SmartCloudApplication.java
+++ src/main/java/com/smartcloud/SmartCloudApplication.java
@@ -28,18 +28,18 @@ public class SmartCloudApplication {
 	@RequestMapping("/")
 
 	public String index(Model model){
-		Person single=new Person("aa",11);
-		List<Person> people=new ArrayList<Person>();
-		Person p1=new Person("xx",11);
-		Person p2=new Person("yy",22);
-		Person p3=new Person("zz",33);
-		people.add(p1);
-		people.add(p2);
-		people.add(p3);
-
-		model.addAttribute("singlePerson",single);
-		model.addAttribute("people",people);
-		return "index";
+//		Person single=new Person("aa",11);
+//		List<Person> people=new ArrayList<Person>();
+//		Person p1=new Person("xx",11);
+//		Person p2=new Person("yy",22);
+//		Person p3=new Person("zz",33);
+//		people.add(p1);
+//		people.add(p2);
+//		people.add(p3);
+//
+//		model.addAttribute("singlePerson",single);
+//		model.addAttribute("people",people);
+		return "login";
 	}
 
 	public static void main(String[] args) {
